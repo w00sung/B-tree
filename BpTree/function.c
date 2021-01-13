d87e4fcc8b05325e02d1145a27d6aeb6fd4b7c65
w00sung <jeongws3240@gmail.com>
2021-01-13 16:38:25 +0900

    DELETE for not enough NON-LEAF Target which has RIGHT sibling

--- BpTree/function.c
+++ BpTree/function.c
@@ -15,7 +15,7 @@ Node* createNode()
 	Node* newNode = (Node*)malloc(sizeof(Node));
 	// 삽입과정에서 필요하여, 하나씩 더 추가
 	Node** children = (Node**)malloc((DEGREE + 2) * sizeof(Node*));
-	int* key = (int*)malloc((DEGREE+1) * sizeof(int));
+	int* key = (int*)malloc((DEGREE + 1) * sizeof(int));
 
 	newNode->C = children;
 	newNode->Key = key;
@@ -27,7 +27,7 @@ Node* createNode()
 int searchNode(Node* root, int k)
 {
 	int idx = (root->N);
-	while (idx > 0 && root->Key[idx-1] > k)
+	while (idx > 0 && root->Key[idx - 1] > k)
 	{
 		idx--;
 	}
@@ -87,7 +87,7 @@ void insertTree(Node** root, int k)
 
 void insertNonfull(Node* root, int k)
 {
-	int idx = (root->N)-1;
+	int idx = (root->N) - 1;
 	if (root->isLeaf)
 	{
 		while ((idx >= 0) && (k < root->Key[idx]))
@@ -134,7 +134,7 @@ void splitChild(Node* parent, int idx)
 
 
 		// 오른쪽에 왼쪽의 뒷값들을 넣어주고
-		for (int i = 0; i < MIN_DEGREE-1; i++)
+		for (int i = 0; i < MIN_DEGREE - 1; i++)
 		{
 			// 왼쪽이 뚱뚱해짐
 			right->Key[i] = left->Key[i + MIN_DEGREE];
@@ -151,7 +151,7 @@ void splitChild(Node* parent, int idx)
 
 		left->N = MIN_DEGREE;
 		left->C[left->N] = right;
-		
+
 
 
 		//(부모) 밀고, 중간자를 올리고, 자식 연결시키고
@@ -171,7 +171,7 @@ void splitChild(Node* parent, int idx)
 		// 부모에 키 삽입
 		parent->Key[idx] = right->Key[0];
 		parent->N++;
-		
+
 	}
 
 	//리프가 아니면, (B TREE 와 동일 )
@@ -240,135 +240,202 @@ void printAll(Node* root, int depth)
 
 void deleteTree(Node** root, int k)
 {
-	int isIn = searchNode(*root, k);
+
 	// 안에 없을 때만 삭제 시작
-	if (isIn) 
+
+	int idx = (*root)->N;
+
+	// 나보다 다 크면, 0에 서게 됨
+	// 내가 제일 크면 N에 서있음
+	while (idx > 0 & (*root)->Key[idx - 1] > k)
 	{
-		int idx = (*root)->N;
-
-		// 나보다 다 크면, 0에 서게 됨
-		// 내가 제일 크면 N에 서있음
-		while (idx > 0 & (*root)->Key[idx-1] > k)
-		{
-			idx--;
-		}
-
-		const int goal_idx = idx;
-
-		// 들어온 너 리프야?
-		if ((*root)->isLeaf)
-		{
-			// 나는 Key[goal_idx - 1]이다.
-			for (int i = goal_idx-1; i < ((*root)->N)-1; i++)
-			{
-				(*root)->Key[i] = (*root)->Key[i + 1];
-			}
-			(*root)->N--;
-		}
-		// 리프아니야 ( == 나 더 들어갈 수 있어 !!)
-		else
-		{
-			Node* Target = (*root)->C[goal_idx];
-			if (Target->N < MIN_DEGREE)
-			{
-				// 자식이 안뚱뚱한데, 리프야
-				if (Target->isLeaf)
-				{	
-					// 오른쪽 끝에 서있으면, 무조건 왼쪽 형제가 형제다.
-					if (goal_idx == (*root)->N)
-					{
-						// 빌려온다 -> 끝값을 빌려오고, 부모를 업데이트 해줘야함
-						Node* Sibling = (*root)->C[goal_idx - 1];
-						if (Sibling->N >= MIN_DEGREE)
-						{
-							for (int i = Target->N; i >0; i--)
-							{
-								Target->Key[i] = Target->Key[i-1];
-							}
-							Target->Key[0] = Sibling->Key[(Sibling->N) - 1];
-							Target->N++;
-							Sibling->N--;
-
-							(*root)->Key[goal_idx - 1] = Target->Key[0];
-						}
-						else
-						{
-							// 합친다.
-							for (int i = 0; i < Target->N; i++)
-							{
-								Sibling->Key[(Sibling->N)+i] = Target->Key[i];
-
-							}
-							Sibling->N = Sibling->N + Target->N;
-							free(Target);
-							Target = Sibling;
-							(*root)->N--;
-							if ((*root)->N == 0)
-							{
-								printf("root가 바뀝니다 \n");
-								*root = Target;
-							}
-						}
-
-					}
-
-					// 그 외에는, 무조건 오른쪽 형제가 형제다.
-					else
-					{
-						Node* Sibling = (*root)->C[goal_idx + 1];
-						// 형제 뚱뚱해 ? -> 빌려올거야
-						if (Sibling->N >= MIN_DEGREE)
-						{
-							Target->Key[Target->N] = (*root)->Key[goal_idx];
-							Target->N++;
-							// 끝 Child 에 Sibling 연결시켜준다.
-							Target->C[(Target->N)] = Sibling ;
-								
-							// 형제의 키 값 당긴다.
-							for (int i = 0; i < (Sibling->N)-1; i++)
-							{
-								Sibling->Key[i] = Sibling->Key[i + 1];
-							
-							}
-							Sibling->N--;
-							(*root)->Key[goal_idx] = Sibling->Key[0];
-						}
-						// 형제도 안뚱뚱해 -> 합칠거야
-						else
-						{
-							for (int i = 0; i < MIN_DEGREE-1; i++)
-							{
-								Target->Key[(MIN_DEGREE-1) + i] = Sibling->Key[i];
-							}
-							Target->N = Target->N + Sibling->N;
-							Target->C[Target->N] = Sibling->C[Sibling->N];
-							
-
-							for (int i = goal_idx - 1; i < ((*root)->N)-1; i++)
-							{
-								(*root)->Key[i] = (*root)->Key[i + 1];
-								(*root)->C[i + 1] = (*root)->C[i + 2];
-							}
-							(*root)->N--;
-							if ((*root)->N == 0)
-							{
-								printf("root가 바뀝니다 \n");
-								*root = Target;
-							}
-						}
-					}
-				}
-				// 자식이 안뚱뚱한데, 리프가 아니야
-				else
-				{
-
-				}
-			}
-			deleteTree(&Target, k);
-		}
+		idx--;
 	}
+
+	const int goal_idx = idx;
+
+	// 들어온 너 리프야?
+	if ((*root)->isLeaf)
+	{
+		// 나는 Key[goal_idx - 1]이다.
+		for (int i = goal_idx - 1; i < ((*root)->N) - 1; i++)
+		{
+			(*root)->Key[i] = (*root)->Key[i + 1];
+		}
+		(*root)->N--;
+	}
+	// 리프아니야 ( == 나 더 들어갈 수 있어 !!)
 	else
 	{
-		printf("The key is not in the Tree");
+		Node* Target = (*root)->C[goal_idx];
+		if (Target->N < MIN_DEGREE)
+		{
+			// 자식이 안뚱뚱한데, 리프야
+			if (Target->isLeaf)
+			{
+				// 오른쪽 끝에 서있으면, 무조건 왼쪽 형제가 형제다.
+				if (goal_idx == (*root)->N)
+				{
+					// 빌려온다 -> 끝값을 빌려오고, 부모를 업데이트 해줘야함
+					Node* Sibling = (*root)->C[goal_idx - 1];
+					if (Sibling->N >= MIN_DEGREE)
+					{
+						for (int i = Target->N; i > 0; i--)
+						{
+							Target->Key[i] = Target->Key[i - 1];
+						}
+						Target->Key[0] = Sibling->Key[(Sibling->N) - 1];
+						Target->N++;
+						Sibling->N--;
+
+						(*root)->Key[goal_idx - 1] = Target->Key[0];
+					}
+					else
+					{
+						// 합친다.
+						for (int i = 0; i < Target->N; i++)
+						{
+							Sibling->Key[(Sibling->N) + i] = Target->Key[i];
+
+						}
+						Sibling->N = Sibling->N + Target->N;
+						free(Target);
+						Target = Sibling;
+						(*root)->N--;
+						if ((*root)->N == 0)
+						{
+							printf("root가 바뀝니다 \n");
+							*root = Target;
+						}
+					}
+
+				}
+
+				// 그 외에는, 무조건 오른쪽 형제가 형제다.
+				else
+				{
+					Node* Sibling = (*root)->C[goal_idx + 1];
+					// 형제 뚱뚱해 ? -> 빌려올거야
+					if (Sibling->N >= MIN_DEGREE)
+					{
+						Target->Key[Target->N] = (*root)->Key[goal_idx];
+						Target->N++;
+						// 끝 Child 에 Sibling 연결시켜준다.
+						Target->C[(Target->N)] = Sibling;
+
+						// 형제의 키 값 당긴다.
+						for (int i = 0; i < (Sibling->N) - 1; i++)
+						{
+							Sibling->Key[i] = Sibling->Key[i + 1];
+
+						}
+						Sibling->N--;
+						(*root)->Key[goal_idx] = Sibling->Key[0];
+					}
+					// 형제도 안뚱뚱해 -> 합칠거야
+					else
+					{
+						for (int i = 0; i < Sibling->N; i++)
+						{
+							Target->Key[(Target->N) + i] = Sibling->Key[i];
+						}
+						Target->N = Target->N + Sibling->N;
+						Target->C[Target->N] = Sibling->C[Sibling->N];
+
+
+						for (int i = goal_idx - 1; i < ((*root)->N) - 1; i++)
+						{
+							(*root)->Key[i] = (*root)->Key[i + 1];
+							(*root)->C[i + 1] = (*root)->C[i + 2];
+						}
+						(*root)->N--;
+						if ((*root)->N == 0)
+						{
+							printf("root가 바뀝니다 \n");
+							*root = Target;
+						}
+					}
+				}
+			}
+			// 자식이 안뚱뚱한데, 리프가 아니야
+			else
+			{
+				// 오른쪽 끝에 서있다.
+				if (goal_idx == (*root)->N)
+				{
+					Node* Sibling = (*root)->C[goal_idx - 1];
+					// 형제 뚱뚱해
+					if (Sibling->N >= MIN_DEGREE)
+					{
+						// 빌릴거야
+					}
+					// 형제 안뚱뚱해
+					else
+					{
+						// 합칠거야
+					}
+				}
+				// 그 외
+				else
+				{
+					Node* Sibling = (*root)->C[goal_idx + 1];
+					if (Sibling->N >= MIN_DEGREE)
+					{
+						// 빌릴거야
+						Target->Key[Target->N] = (*root)->Key[goal_idx];
+						Target->N++;
+						// 자식까지 보낸다.
+						Target->C[(Target->N)] = Sibling->C[0];
+
+						(*root)->Key[goal_idx] = Sibling->Key[0];
+
+						// 형제의 키 값 당긴다.
+						for (int i = 0; i < (Sibling->N) - 1; i++)
+						{
+							Sibling->Key[i] = Sibling->Key[i + 1];
+						}
+						
+						// 형제의 C 당긴다.
+						for (int i = 0; i < Sibling->N; i++)
+						{
+							Sibling->C[i] = Sibling->C[i + 1];
+						}
+						Sibling->N--;
+					}
+					// 형제 안뚱뚱해
+					else
+					{
+						// 합칠거야
+						// 리프가 아니면 부모도 넣을거야
+						Target->Key[Target->N] = (*root)->Key[goal_idx-1];
+						for (int i = 0; i < Sibling->N; i++)
+						{
+							Target->Key[((Target->N) + 1) + i] = Sibling->Key[i];
+						}
+
+						for (int i = 0; i <= Sibling->N; i++)
+						{
+							Target->C[((Target->N) + 1) + i] = Sibling->C[i];
+						}
+						Target->N = Target->N + 1 + Sibling->N;
+
+						for (int i = goal_idx - 1; i < ((*root)->N) - 1; i++)
+						{
+							(*root)->Key[i] = (*root)->Key[i + 1];
+							(*root)->C[i + 1] = (*root)->C[i + 2];
+						}
+						(*root)->N--;
+						if ((*root)->N == 0)
+						{
+							printf("root가 바뀝니다 \n");
+							*root = Target;
+						}
+						
+					}
+				}
+			}
+		}
+		deleteTree(&Target, k);
 	}
+
 }
